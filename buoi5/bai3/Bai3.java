1cc922b9903efec3196ed27cacc77edc5548e5d3
unknown <dinhthi1440@gmail.com>
2022-08-11 14:10:11 +0700

    Buoi5

--- buoi5/bai3/Bai3.java
+++ buoi5/bai3/Bai3.java
@@ -48,7 +48,6 @@ public class Bai3 {
         }
 
     }
-
     public static boolean isPrime(double n) {
         if (n < 2) {
             return false;
@@ -65,16 +64,42 @@ public class Bai3 {
         }
     }
 
-    public static void check() {
-        if (isPrime(5.77) == false) {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao int n1: ");
+        int n1 = sc.nextInt();
+        System.out.println("Nhap vao long n2: ");
+        long n2 = sc.nextLong();
+        System.out.println("Nhap vao float n3: ");
+        float n3 = sc.nextFloat();
+        System.out.println("Nhap vao double n4: ");
+        double n4 = sc.nextDouble();
+        System.out.println("Voi int n1 = "+ n1);
+        if (isPrime(n1) == false) {
             System.out.println("Day khong phai so nguyen to !");
         } else {
             System.out.println("Day la so nguyen to !");
         }
-    }
-
-    public static void main(String[] args) {
-        check();
+        System.out.println("Voi long n2 = "+ n2);
+        if (isPrime(n2) == false) {
+            System.out.println("Day khong phai so nguyen to !");
+        } else {
+            System.out.println("Day la so nguyen to !");
+        }
+        System.out.println("Voi float n3 = "+ n3);
+        if (isPrime(n3) == false) {
+            System.out.println("Day khong phai so nguyen to !");
+        } else {
+            System.out.println("Day la so nguyen to !");
+        }
+        System.out.println("Voi double n4 = "+ n4);
+        if (isPrime(n4) == false) {
+            System.out.println("Day khong phai so nguyen to !");
+        } else {
+            System.out.println("Day la so nguyen to !");
+        }
     }
 
 }
